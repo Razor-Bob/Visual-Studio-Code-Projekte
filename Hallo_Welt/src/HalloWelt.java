b1928796d420d102480e905d0b5ff082f75d78fe
Razor-Bob <ramon.prause@proton.me>
2026-07-11 13:00:27 +0200

    Änderung Kommentar

--- Hallo_Welt/src/HalloWelt.java
+++ Hallo_Welt/src/HalloWelt.java
@@ -1,6 +1,6 @@
 public class HalloWelt {
     public static void main(String[] args) 
     {
-        System.out.println("Hallo Welt!"); // Ausgabe von "Hallo Welt!" auf der Konsole
+        System.out.println("Hallo Welt!"); // Ausgabe von "Hallo Welt!" auf der Konsole TEST
     }
 }
