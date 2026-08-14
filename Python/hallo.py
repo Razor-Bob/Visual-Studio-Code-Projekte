f511571e0dbeeb4ceb757f1176bd12acebbdb30d
Razor-Bob <ramon.prause@proton.me>
2026-08-14 07:55:03 +0200

    Übungen Variablen und Datentypen

--- Python/hallo.py
+++ Python/hallo.py
@@ -1,1 +1,15 @@
-print("Hallo Welt! Python funktioniert.")
+# Die print-Funktion gibt die Nachricht "Hallo Python!" aus. Die len-Funktion gibt die Länge der Nachricht aus.
+# print(len("Hallo Python!"))
+# Die Methode upper() wandelt alle Buchstaben in Großbuchstaben um.
+# text = "Hallo Python!"
+# grossbuchstaben = text.upper()
+# print(grossbuchstaben)
+# Die Methode lower() wandelt alle Buchstaben in Kleinbuchstaben um.
+# text = "Hallo Python!"
+# kleinbuchstaben = text.lower()
+# print(kleinbuchstaben)
+# Die Methode isupper() überprüft, ob alle Buchstaben in einem String Großbuchstaben sind.
+# text = "Hallo Python!"
+# print(text.isupper())  # Gibt False zurück, da nicht alle Buchstaben Großbuchstaben sind.
+# Aufrufen der Methode isupper direkt auf dem String "Hallo Python!" und Ausgabe des Ergebnisses.
+print("Hallo Python!".isupper())  # Gibt False zurück, da nicht alle Buchstaben
